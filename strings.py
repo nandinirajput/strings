037865b35e771a7a0b7d00ac037f4b719fd83392
nandinirajput <109655286+nandinirajput@users.noreply.github.com>
2022-07-21 22:01:35 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,42 @@
+# print(type(5.6))
+# print(type(5))
+
+# print('hello world')
+# print("hello world")
+# print('''hello world''')
+# print("""hello world""")
+
+
+#''''working with int,float and string''''
+# printprint(54+2.1)
+# print(type(26+3.8))
+# (type(6.23+3.25))
+# print(type(6.4+2.6))
+# print('256'+2.8)
+# print('54'+54)
+
+# print('hello'+54)
+# print('hello'+2.5)
+
+# print(1,2,3,4,5,6)
+# print(1,2,3,4,5,sep='')
+# print(1,2,3,4,5,sep=' ')
+# print(1,2,3,4,5,sep='   ')
+# print(1,2,3,4,5,sep='-')
+
+# print('hello'+'world')
+# print('hello '+'world')
+# print('hello'+' world')
+# print('hello','world')
+# print('hi','rajput')
+
+# print('hello'+''+'morning')
+# print('hello'+' '+'morning')
+# print("hello"+" "+"morning")
+# print("hello"+"-"+"morning")
+
+# print('python'*5)
+# print('python '*4)
+# print('python/n'*3)
+# print('python/n'*4 ,end='')
+# print('python/n'*6,end=' ')
